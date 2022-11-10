5ecd8e0579c4e46c64822e42de3d01a98da91f47
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-11-10 18:02:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,25 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
-cin >> a >> b >> c;
+  float a;
+  float b;
+  float c;
+  cout << "Inserisci le variabili a,b,c" << endl;
+  cout << "a: ";
+  cin >> a;
+  cout << "b: ";
+  cin>> b;
+  cout << "c: ";
+  cin >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+  cout<< "Area del triangolo: ";
+  cout<< (a*b)/2 <<endl;
+  cout<<"Area del quadrato: ";
+  cout<< a*a <<endl;
+  cout<< "Area del rettangolo: ";
+  cout<< a*b <<endl;
+  cout<< "Area del trapezio: ";
+  cout<< (((a+b)*c)/2) <<endl;
+return 0;
 }
